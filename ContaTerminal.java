b249888960eeacf945592e8d4eb14f10b1572606
EwerttonSilva <ewertton.sllv1@gmail.com>
2025-02-09 19:42:42 -0300

    commit inicial

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = input.nextLine();
+
+        System.out.println("Informe a sua data de nascimento: ");
+        String nascimento = input.nextLine();
+
+        String mensagem = String.format("Olá, %s! sua conta foi criada com sucesso. Data de Nascimento: %s", nome, nascimento);
+        System.out.println(mensagem);
+    }
 }
--- out/production/Dio-Desafio-BancoTerminal/README.md
+++ out/production/Dio-Desafio-BancoTerminal/README.md
@@ -1,3 +1,2 @@
 # Dio-Desafio-BancoTerminal
 Desafio de criar um banco via terminal.
-Estou digitiando isso pelo BASH
